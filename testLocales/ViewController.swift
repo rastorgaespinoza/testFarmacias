7700c6a814780745a09f9619b2e4a07985602b09
Rodrigo <r.astorgaespinoz@gmail.com>
2020-08-24 19:35:38 -0400

    Initial Commit

---
+++ testLocales/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testLocales
+//
+//  Created by Rodrigo Astorga on 24-08-20.
+//  Copyright © 2020 Rodrigo Astorga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
